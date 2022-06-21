e7482345af5c5e479cdce43d7835f93de92baefc
yuri818 <2yooree98@naver.com>
2022-06-21 09:42:49 +0900

    git

---
+++ hello.js
@@ -0,0 +1,7 @@
+let colors = require('colors');
+
+console.log(colors.rainbow('Hello NodeJS!!!'));
+console.log(colors.green('Hello NodeJS!!!'));
+console.log(colors.red('Hello NodeJS!!!'));
+let a = 5;
+console.log(a);
