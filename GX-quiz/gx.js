4c3b38250b641b5c44dbe056c0f0676cb876eac5
yuri818 <2yooree98@naver.com>
2022-07-15 15:18:40 +0900

    20220715

--- GX-quiz/gx.js
+++ GX-quiz/gx.js
@@ -1,3 +1,14 @@
+const items = document.querySelector(".items");
 const item_row = document.querySelector(".item_row");
-const btn_add = document.querySelector(".btn_row");
-const item_delete = document.querySelector(".item_")
+const item = document.querySelector(".item");
+const item_name = document.querySelector(".item_name");
+const item_delete = document.querySelector(".item_delete");
+const footer_input = document.querySelector(".footer_input");
+const btn_add = document.querySelector(".btn_add");
+
+btn_add.addEventListener('click', () => {
+  if(footer_input.length > 0)
+  item.appendChild(document.createTextNode(footer_input.value));
+  // item_row.appendChild('item');
+  // items.appendChild('item_row');
+});
